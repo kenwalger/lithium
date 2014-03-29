1bd2654c2764630f90f52f74065e7fcf6a66ad18
Thomas Foster <thomasjamesfoster@bigpond.com>
2014-03-29 13:49:40 +1100

    Update lithium.sh

--- src/lithium.sh
+++ src/lithium.sh
@@ -1,1 +1,15 @@
-###
+#!/bin/bash
+
+if [ $1 -eq "--help" ]
+then
+  /etc/lithium/help.sh $2 $3
+elif [ $1 -eq "--compile" ]
+then
+  
+elif [ $1 -eq "--translate" ]
+then
+  
+elif [ $1 -eq "--run" ]
+then
+  
+fi
