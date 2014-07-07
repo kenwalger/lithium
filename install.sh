c63202d8f89e8386d40285bd75a48ede6da40a75
Thomas Foster <thomasjamesfoster@bigpond.com>
2014-07-07 13:53:00 +1000

    move the install script.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Go to /etc and make a place for lithium config files, symlinks to modules and other stuff.
+
+cd /etc
+
+mkdir lithium
+
+cd lithium
+
+mkdir source
+mkdir 
+
+# Go to the 
+
+cd /usr/lib
+
+mkdir lithium
+
+
+
+cp /src/lithium.sh /bin/lithium
+
+chmod +x /bin/lithium
--- src/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /etc
-
-mkdir lithium
-
-cp ./src/lithium.sh /bin/lithium
-
-chmod +x /bin/lithium
