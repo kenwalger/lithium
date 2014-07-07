a29be5c4f3ed03c265080245b6fe15d3ece1acb2
Thomas Foster <thomasjamesfoster@bigpond.com>
2014-07-07 14:09:32 +1000

    Updated install script and .travis.yml

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+## Linux
+LSOF=$(lsof -p $$ | grep -E "/"$(basename $0)"$")
+MY_PATH=$(echo $LSOF | sed -r s/'^([^\/]+)\/'/'\/'/1 2>/dev/null)
+if [ $? -ne 0 ]; then
+## OSX
+  MY_PATH=$(echo $LSOF | sed -E s/'^([^\/]+)\/'/'\/'/1 2>/dev/null)
+fi
+
+MY_ROOT=$(dirname $MY_PATH)
+
+LIB="$MY_ROOT/lib/*"
+
 # Go to /etc and make a place for lithium config files, symlinks to modules and other stuff.
 
 cd /etc
@@ -9,16 +21,13 @@ mkdir lithium
 cd lithium
 
 mkdir source
-mkdir 
+mkdir packages
+mkdir
 
-# Go to the 
+# Go to /usr/lib and make a place to store modules.
 
 cd /usr/lib
 
 mkdir lithium
 
-
-
-cp /src/lithium.sh /bin/lithium
-
-chmod +x /bin/lithium
+cp $LIB /usr/lib/lithium/
