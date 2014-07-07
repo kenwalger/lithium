422acc61f0ef38a4395e01c7acb09c07d4bf4dcb
Thomas Foster <thomasjamesfoster@bigpond.com>
2014-07-07 14:25:24 +1000

    Condense install script.

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 ## Linux
-LSOF=$(lsof -p $$ | grep -E "/"$(basename $0)"$")
-MY_PATH=$(echo $LSOF | sed -r s/'^([^\/]+)\/'/'\/'/1 2>/dev/null)
+ROOT=$(dirname $(echo $(lsof -p $$ | grep -E "/"$(basename $0)"$") | sed -r s/'^([^\/]+)\/'/'\/'/1 2>/dev/null))
 if [ $? -ne 0 ]; then
 ## OSX
-  MY_PATH=$(echo $LSOF | sed -E s/'^([^\/]+)\/'/'\/'/1 2>/dev/null)
+  ROOT=$(dirname $(echo $(lsof -p $$ | grep -E "/"$(basename $0)"$") | sed -E s/'^([^\/]+)\/'/'\/'/1 2>/dev/null))
 fi
-
-MY_ROOT=$(dirname $MY_PATH)
-
-LIB="$MY_ROOT/lib/*"
 
 # Go to /etc and make a place for lithium config files, symlinks to modules and other stuff.
 
@@ -22,7 +17,18 @@ cd lithium
 
 mkdir source
 mkdir packages
-mkdir
+mkdir compilers
+mkdir docs
+mkdir settings
+mkdir bin
+
+cp $ROOT/docs/* /etc/lithium/docs/
+
+cp $ROOT/src/* /etc/lithium/bin/
+
+cd source
+
+git clone https://github.com/thomasfoster96/lithium.git
 
 # Go to /usr/lib and make a place to store modules.
 
@@ -30,4 +36,4 @@ cd /usr/lib
 
 mkdir lithium
 
-cp $LIB /usr/lib/lithium/
+cp $ROOT/lib/* /usr/lib/lithium/
