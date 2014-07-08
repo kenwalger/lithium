0a291713f2a057c7aeb1fca94b8fad4adb724e46
Thomas Foster <thomasjamesfoster@bigpond.com>
2014-07-08 16:02:56 +1000

    Rewrite tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,22 +1,28 @@
 import unittest
+import os
 
-class PypkgTest(unittest.TestCase):
+class TrueTest(unittest.TestCase):
     
     def test_true(self):
         self.assertTrue(True)
 
-class CoreTest(unittest.TestCase):
+class HelpTest(unittest.TestCase):
 
-	def setUp(self):
-		pass
+	def test_help(self):
 
-	def tearDown(self):
-		pass
+		expectation = ["Lithium Version 0.0.1 Help", "", "For help type:", "", \
+			"--feature <feature> for help with a particular Lithium feature", \
+		 	"", "--package <name> for help with a Lithium package", "", \
+			"--error <number> for help with an error message", "", \
+			"--action <task> for help with a common task, such as translating",\
+			 "", "--quit to quit Lithium help", ""
 
-	def test_echo(self):
-		self.assertEqual("ping", "ping")
-		self.assertEqual([1,2], [1,2])
-		self.assertEqual({1:2}, {1:2})
+		reality = os.popen("lithium help").readlines()
+
+		for i in range(len(reality)):
+			reality[i] = reality[i].strip()
+
+		self.assertEqual(expectation, reality)
 
 if __name__ == "__main__":
 	unittest.main()
