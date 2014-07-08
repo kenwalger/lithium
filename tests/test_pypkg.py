23ab7c7528729d5828a3c90fee01c00d8c7fb136
Thomas Foster <thomasjamesfoster@bigpond.com>
2014-07-08 11:25:29 +1000

    Revert "Remove more python junk"
    This reverts commit 60ee0f994c7c27f5285b6c78b77b1b7a66e121b0.

---
+++ tests/test_pypkg.py
@@ -0,0 +1,24 @@
+import unittest
+
+from src import core
+
+class PypkgTest(unittest.TestCase):
+    
+    def test_true(self):
+        self.assertTrue(True)
+
+class CoreTest(unittest.TestCase):
+	
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_echo(self):
+		self.assertEqual("ping", core.echo("ping"))
+		self.assertEqual([1,2], core.echo([1,2]))
+		self.assertEqual({1:2}, core.echo({1:2}))
+		
+if __name__ == "__main__":
+	unittest.main()
