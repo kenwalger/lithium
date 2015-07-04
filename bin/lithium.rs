dacb1353d80262c363aa5e3987c689cdda2f4cce
Thomas Foster <thomasjamesfoster@bigpond.com>
2015-07-04 23:07:04 +1000

    Create lithium.rs

---
+++ bin/lithium.rs
@@ -0,0 +1,14 @@
+extern crate argparse;
+use argparse::{ArgumentParser, StoreTrue, Store};
+
+fn main() {
+    let mut run = false;
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Compile and run Lithium programs.");
+        ap.refer(&mut verbose)
+            .add_option(&["-r", "--run"], StoreTrue,
+            "Run the specified file.");
+        ap.parse_args_or_exit();
+    }
+}
