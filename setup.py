e2159cc24d08a02155a68cf2febda1b98e4255e2
Thomas Foster <thomasjamesfoster@bigpond.com>
2015-07-04 20:39:20 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name = 'Lithium',
+  version = '0.0.1',
+  author = 'Thomas Foster',
+  url = 'https://github.com/thomasfoster96/lithium/',
+  packages = ['lithium', 'lithium.compile'],
+)
