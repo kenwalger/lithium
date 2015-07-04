595195ea17f6d81fe0a1710416e9813855aba1b7
Thomas Foster <thomasjamesfoster@bigpond.com>
2015-07-04 21:04:56 +1000

    Create lithium.py

---
+++ bin/lithium.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import argparse
+import lithium
+
+argparser = argparse.ArgumentParser()
+
+argparser.add_argument("source", help="Source Lithium file to use.")
+
+method = parser.add_mutually_exclusive_group()
+method.add_argument("-c", "--compile", action="store_true")
+method.add_argument("-r", "--run", action="store_true")
+
+args = argparser.parse_args()
