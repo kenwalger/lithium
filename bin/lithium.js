de8407180052023282af25e1f94b73006497ac55
Thomas Foster <thomasjamesfoster@bigpond.com>
2015-07-15 00:04:04 +1000

    remove other language package files

--- bin/lithium.js
+++ bin/lithium.js
@@ -1,13 +1,5 @@
 #!/usr/bin/env node
 
 import colors from 'colors/safe';
-import ArgumentParser from 'argparse';
-
 import * as lithium from '../src/index.js';
-
-const parser = new ArgumentParser();
-const subparsers = parser.addSubparsers({
-	title : 'sub-commands',
-
-});
 
--- lithium.nimble
+++
@@ -1,9 +0,0 @@
-[Package]
-name = "Lithium"
-version = 0.0.1
-author = "Thomas Foster"
-description = "The Lithium programming langauge."
-license = "MIT"
-
-[Deps]
-requires = nim >= 0.10.0
