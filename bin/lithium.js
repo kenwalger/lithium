ff1ca4efde94dc211c30117aa7cfdac3abf98261
Thomas Foster <thomasjamesfoster@bigpond.com>
2015-07-15 00:50:03 +1000

    very unpythonic

--- bin/lithium.js
+++ bin/lithium.js
@@ -1,5 +1,47 @@
 #!/usr/bin/env node
 
-import colors from 'colors/safe';
 import * as lithium from '../src/index.js';
 
+let rawArgs = process.argv.slice(2);
+let parsedArgs = {};
+let whatToDo = 'repl';
+
+switch(rawArgs[0]){
+
+	// Install a package/toolkit.
+	case "install":
+		rawArgs.shift();
+		if(!rawArgs[0].match(/^--.*$/)){
+			parsedArgs["package"] = rawArgs.shift();
+		}
+		break;
+
+	// User wants to run or compile a Lithium file.
+	case "run":
+	case "compile":
+		rawArgs.shift();
+		if(!rawArgs[0].match(/^--.*$/)){
+			parsedArgs["file"] = rawArgs.shift();
+		}
+		break;
+
+	// Bare lithium command means open a repl or, if a file is supplied, run a file.
+	default:
+		if(!rawArgs[0].match(/^--.*$/)){
+			parsedArgs["file"] = rawArgs.shift();
+			whatToDo = 'run';
+		} else {
+			whatToDo = 'repl';
+		}
+
+}
+
+let i = 0;
+while(rawArgs.length > 0){
+	if(rawArgs[i].match(/^--.*$/)){
+		parsedArgs[rawArgs[i].slice(2)] = rawArgs[i+1].match(/^--.*$/) ? (i++, true) : rawArgs[i+1];
+	} 
+	i++;
+}
+
+lithium[whatToDo](parsedArgs);
--- bin/lithium.py
+++ bin/lithium.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python
 
-import argparse
 import lithium
+import sys
 
-argparser = argparse.ArgumentParser()
+rawArgs = sys.argv[1:]
+parsedArgs = {}
+whatToDo = 'repl'
+option = re.compile('^--.*$')
 
-argparser.add_argument("source", help="Source Lithium file to use.")
+if rawArgs[0] == "install":
+	whatToDo = rawArgs.pop(0)
+	if not option.match(rawArgs[0])
+		parsedArgs['package'] = rawArgs.pop(0)
+elif rawArgs[0] == "run" or rawArgs[0] == "compile"
+	whatToDp = rawArgs.pop(0)
+	if not option.match(rawArgs[0])
+		parsedArgs['file'] = rawArgs.pop(0)
+else
+	if not option.match(rawArgs[0])
+		whatToDo = 'run'
+		parsedArgs['file'] = rawArgs.pop(0)
+	else 
+		whatToDo = 'repl'
 
-method = parser.add_mutually_exclusive_group()
-method.add_argument("-c", "--compile", action="store_true")
-method.add_argument("-r", "--run", action="store_true")
+i = 0
 
-args = argparser.parse_args()
+while len(rawArgs) > 0
+	if option.match(rawArgs[i])
+		if option.match(rawArgs[i+1])
+			parsedArgs[rawArgs[i][2:]] = True
+			i += 1
+		else
+			parsedArgs[rawArgs[i][2:]] = rawArgs[i+1]
+			i += 2
 
+lithium[whatToDo](parsedArgs)
