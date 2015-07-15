5cc74c5181770bf30afac5dc44c9ac4031d5dd23
Thomas Foster <thomasjamesfoster@bigpond.com>
2015-07-15 23:53:03 +1000

    add some more python files

--- bin/lithium.rb
+++ bin/lithium.rb
@@ -5,25 +5,39 @@ require 'lithium'
 rawArgs = ARGV
 parsedArgs = {}
 whatToDo = ''
+option = /^--.*$/
 
 case rawArgs[0]
 when 'install'
+	whatToDo = rawArgs.shift
+	if !option.match(rawArgs[0])
+		parsedArgs['package'] = rawArgs.shift
 when 'run'
-
+	whatToDo = rawArgs.shift
+	if !option.match(rawArgs[0])
+		parsedArgs['file'] = rawArgs.shift
 when 'compile'
+	whatToDo = rawArgs.shift
+	if !option.match(rawArgs[0])
+		parsedArgs['file'] = rawArgs.shift
 else
-	if
+	if !option.match(rawArgs[0])
 		whatToDo = 'run'
-		parsedArgs['file'] = 
+		parsedArgs['file'] = rawArgs.shift
 	else
+		whatToDo = 'repl'
 	end
 end
 
+i = 0
 while rawArgs.length > 0
-	if 
-		if condition
-			
+	if option.match(rawArgs[i])
+		if option.match(rawArgs[i+1])
+			parsedArgs[rawArgs[i][2..rawArgs[i].size]] = true
+			i += 1
 		else
+			parsedArgs[rawArgs[i][2..rawArgs[i].size]] = rawArgs[i+1]
+			i += 2
 		end
 	end
 end
--- src/lithium.rb
+++ src/lithium.rb
@@ -1,3 +1,19 @@
 class Lithium
+	
 	def self.run()
+
+	end
+
+	def self.compile()
 		
+	end
+
+	def self.repl()
+
+	end
+
+	def self.install()
+
+	end
+
+end		
