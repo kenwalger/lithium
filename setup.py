dc31cd12fa7e9fde4b2fd1fd2f435b6a2795d27d
Thomas Foster <thomasjamesfoster@bigpond.com>
2015-08-05 23:18:44 +1000

    move python to src/

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
   name = 'Lithium',
   version = '0.0.1',
   author = 'Thomas Foster',
+  author_email = 'thomasjamesfoster@bigpond.com',
   url = 'https://github.com/thomasfoster96/lithium/',
   packages = ['lithium', 'lithium.compile'],
+  package_dir = {'lithium' : 'src'},
+  scripts = ['bin/lithium']
 )
